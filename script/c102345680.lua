f6e6b71c81abb9a9a7eb5062ee2dd1ab95964456
Guillaume Roux <rouxguillaume8@gmail.com>
2025-03-10 10:51:54 +0100

    feat: more effects to Ra

--- script/c102345680.lua
+++ script/c102345680.lua
@@ -34,6 +34,41 @@ function s.initial_effect(c)
 	e6:SetTarget(s.tgtg)
 	e6:SetOperation(s.tgop)
 	c:RegisterEffect(e6)
+	--tribute check
+	local e7=Effect.CreateEffect(c)
+	e7:SetType(EFFECT_TYPE_SINGLE)
+	e7:SetCode(EFFECT_MATERIAL_CHECK)
+	e7:SetValue(s.valcheck)
+	c:RegisterEffect(e7)
+	--give atk effect only when summon
+	local e8=Effect.CreateEffect(c)
+	e8:SetType(EFFECT_TYPE_SINGLE)
+	e8:SetCode(EFFECT_SUMMON_COST)
+	e8:SetOperation(s.facechk)
+	e8:SetLabelObject(e7)
+	c:RegisterEffect(e8)
+	--destroy
+	local e9=Effect.CreateEffect(c)
+	e9:SetDescription(aux.Stringid(id,1))
+	e9:SetCategory(CATEGORY_DESTROY)
+	e9:SetType(EFFECT_TYPE_IGNITION)
+	e9:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e9:SetRange(LOCATION_MZONE)
+	e9:SetCost(s.descost)
+	e9:SetTarget(s.destg)
+	e9:SetOperation(s.desop)
+	c:RegisterEffect(e9)
+	--pay atk/def
+	local e10=Effect.CreateEffect(c)
+	e10:SetDescription(aux.Stringid(id,2))
+	e10:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
+	e10:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e10:SetCountLimit(1,0,EFFECT_COUNT_CODE_SINGLE)
+	e10:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
+	e10:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e10:SetCost(s.adcost)
+	e10:SetOperation(s.adop)
+	c:RegisterEffect(e10)
 end
 function s.sumsuc(e,tp,eg,ep,ev,re,r,rp)
 	Duel.SetChainLimitTillChainEnd(aux.FALSE)
@@ -52,5 +87,75 @@ function s.tgop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and c:IsFaceup() then
 		Duel.SendtoGrave(c,REASON_EFFECT)
+	end
+end
+function s.valcheck(e,c)
+	local g=c:GetMaterial()
+	local tc=g:GetFirst()
+	local atk=0
+	local def=0
+	while tc do
+		local catk=tc:GetAttack()
+		local cdef=tc:GetDefense()
+		atk=atk+(catk>=0 and catk or 0)
+		def=def+(cdef>=0 and cdef or 0)
+		tc=g:GetNext()
+	end
+	if e:GetLabel()==1 then
+		e:SetLabel(0)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK)
+		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetValue(atk)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE-RESET_TOFIELD)
+		c:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_SET_DEFENSE)
+		e2:SetValue(def)
+		c:RegisterEffect(e2)
+	end
+end
+function s.facechk(e,tp,eg,ep,ev,re,r,rp)
+	e:GetLabelObject():SetLabel(1)
+end
+function s.descost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,1000) end
+	Duel.PayLPCost(tp,1000)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) end
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
+end
+function s.adcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLP(tp)>100 end
+	local lp=Duel.GetLP(tp)
+	e:SetLabel(lp-100)
+	Duel.PayLPCost(tp,lp-100)
+end
+function s.adop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(e:GetLabel())
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+		c:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_UPDATE_DEFENSE)
+		c:RegisterEffect(e2)
 	end
 end
