b9bd183930062e18998593c58d1a536582a8b4c4
Guillaume Roux <rouxguillaume8@gmail.com>
2025-03-11 13:51:33 +0100

    feat: update Slifer's effect

--- script/c102345679.lua
+++ script/c102345679.lua
@@ -50,7 +50,7 @@ function s.initial_effect(c)
   	--defdown
 	local e9=Effect.CreateEffect(c)
 	e9:SetDescription(aux.Stringid(id,1))
-	e9:SetCategory(CATEGORY_DEFCHANGE+CATEGORY_DESTROY)
+	e9:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE+CATEGORY_DESTROY)
 	e9:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
 	e9:SetRange(LOCATION_MZONE)
 	e9:SetCode(EVENT_SUMMON_SUCCESS)
@@ -96,14 +96,25 @@ function s.defop(e,tp,eg,ep,ev,re,r,rp)
 	local dg=Group.CreateGroup()
 	local c=e:GetHandler()
 	for tc in g:Iter() do
-		local predef=tc:GetDefense()
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_UPDATE_DEFENSE)
-		e1:SetValue(-2000)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		tc:RegisterEffect(e1)
-		if predef~=0 and tc:GetDefense()==0 then dg:AddCard(tc) end
+		if tc:IsPosition(POS_FACEUP_ATTACK) then
+			local preatk=tc:GetAttack()
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetValue(-2000)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e1)
+			if preatk~=0 and tc:GetAttack()==0 then dg:AddCard(tc) end
+		elseif tc:IsPosition(POS_FACEUP_DEFENSE) then
+			local predef=tc:GetDefense()
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_DEFENSE)
+			e1:SetValue(-2000)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e1)
+			if predef~=0 and tc:GetDefense()==0 then dg:AddCard(tc) end
+		end
 	end
 	if #dg==0 then return end
 	Duel.BreakEffect()
