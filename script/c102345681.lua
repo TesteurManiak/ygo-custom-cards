18347fe4c38c50b921208b684f82df3ce8e32c36
Guillaume Roux <rouxguillaume8@gmail.com>
2025-03-12 11:11:51 +0100

    feat: triple tribute for divine beast

--- script/c102345681.lua
+++ script/c102345681.lua
@@ -1,5 +1,16 @@
 -- Nameless Pharaoh
 local s, id = GetID()
 function s.initial_effect(c)
-  -- effects
+  --summon
+  local e1 = Effect.CreateEffect(c)
+  c:RegisterEffect(e1)
+  --triple tribute for divine-beast
+  local e2 = Effect.CreateEffect(c)
+  e2:SetType(EFFECT_TYPE_SINGLE)
+  e2:SetCode(EFFECT_TRIPLE_TRIBUTE)
+  e2:SetValue(s.ttcon)
+  c:RegisterEffect(e2)
+end
+function s.ttcon(e,c)
+  return c:IsRace(RACE_DIVINE)
 end
