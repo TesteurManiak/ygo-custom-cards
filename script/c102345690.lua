bf7ebcb6d0a99f6fc750cb9f13c6028b0a55d49f
Guillaume Roux <rouxguillaume8@gmail.com>
2026-04-07 09:30:42 +0200

    feat: handle stat boosting effect for swordsman

--- script/c102345690.lua
+++ script/c102345690.lua
@@ -1,5 +1,16 @@
 -- White Knight Swordsman
 local s, id = GetID()
 function s.initial_effect(c)
-  -- effects
+	--atk/def up
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetRange(LOCATION_GRAVE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetTargetRange(LOCATION_MZONE,0)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x155d))
+	e1:SetValue(300)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_UPDATE_DEFENSE)
+	c:RegisterEffect(e2)
 end
