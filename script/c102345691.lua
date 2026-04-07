9fae809eab7411966632548f9a8ee025405f3122
Guillaume Roux <rouxguillaume8@gmail.com>
2026-04-07 16:01:26 +0200

    fix: sealed gate considered as white knight card

--- script/c102345691.lua
+++ script/c102345691.lua
@@ -2,6 +2,13 @@
 local SET_WHITE_KNIGHT = 0x155d
 local s, id = GetID()
 function s.initial_effect(c)
+  -- always treated as a White Knight card
+  local e0 = Effect.CreateEffect(c)
+  e0:SetType(EFFECT_TYPE_SINGLE)
+  e0:SetCode(EFFECT_ADD_SETCODE)
+  e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+  e0:SetValue(SET_WHITE_KNIGHT)
+  c:RegisterEffect(e0)
   -- activate
   local e1 = Effect.CreateEffect(c)
   e1:SetType(EFFECT_TYPE_ACTIVATE)
